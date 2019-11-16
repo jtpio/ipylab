e72cb090b0934decd80a2ba00fe3f9557912489e
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2019-11-17 00:16:23 +0100

    Add SplitPanel

--- ipylab/__init__.py
+++ ipylab/__init__.py
@@ -5,7 +5,7 @@
 # Distributed under the terms of the Modified BSD License.
 
 from .jupyterfrontend import JupyterFrontEnd
-from .widgets import Panel
+from .widgets import Panel, SplitPanel
 
 from ._version import __version__, version_info
 
--- ipylab/widgets.py
+++ ipylab/widgets.py
@@ -10,3 +10,17 @@ class Panel(VBox):
     _model_name = Unicode('PanelModel').tag(sync=True)
     _model_module = Unicode(module_name).tag(sync=True)
     _model_module_version = Unicode(module_version).tag(sync=True)
+
+
+class SplitPanel(Panel):
+    _model_name = Unicode('SplitPanelModel').tag(sync=True)
+    _model_module = Unicode(module_name).tag(sync=True)
+    _model_module_version = Unicode(module_version).tag(sync=True)
+    _view_name = Unicode('SplitPanelView').tag(sync=True)
+    _view_module = Unicode(module_name).tag(sync=True)
+    _view_module_version = Unicode(module_version).tag(sync=True)
+
+    orientation = Unicode('vertical').tag(sync=True)
+
+    def addWidget(self, widget):
+        self.children = list(self.children) + [widget]
