828345f48c0487632899768d256c6c5bb1ec9464
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2019-11-18 19:44:40 +0100

    Add black and pre-commit

--- setup.py
+++ setup.py
@@ -10,102 +10,97 @@ from os.path import join as pjoin
 
 
 from setupbase import (
-    create_cmdclass, install_npm, ensure_targets,
-    find_packages, combine_commands, ensure_python,
-    get_version, HERE
+    create_cmdclass,
+    install_npm,
+    ensure_targets,
+    find_packages,
+    combine_commands,
+    ensure_python,
+    get_version,
+    HERE,
 )
 
 from setuptools import setup
 
 
 # The name of the project
-name = 'ipylab'
+name = "ipylab"
 
 # Ensure a valid python version
-ensure_python('>=3.4')
+ensure_python(">=3.4")
 
 # Get our version
-version = get_version(pjoin(name, '_version.py'))
+version = get_version(pjoin(name, "_version.py"))
 
-lab_path = pjoin(HERE, name, 'labextension')
+lab_path = pjoin(HERE, name, "labextension")
 
 # Representative files that should exist after a successful build
 jstargets = [
-    pjoin(HERE, 'lib', 'plugin.js'),
+    pjoin(HERE, "lib", "plugin.js"),
 ]
 
-package_data_spec = {
-    name: [
-        'labextension/*.tgz'
-    ]
-}
+package_data_spec = {name: ["labextension/*.tgz"]}
 
 data_files_spec = [
-    ('share/jupyter/lab/extensions', lab_path, '*.tgz'),
+    ("share/jupyter/lab/extensions", lab_path, "*.tgz"),
 ]
 
 
-cmdclass = create_cmdclass('jsdeps', package_data_spec=package_data_spec,
-    data_files_spec=data_files_spec)
-cmdclass['jsdeps'] = combine_commands(
-    install_npm(HERE, build_cmd='build'),
-    ensure_targets(jstargets),
+cmdclass = create_cmdclass(
+    "jsdeps", package_data_spec=package_data_spec, data_files_spec=data_files_spec
+)
+cmdclass["jsdeps"] = combine_commands(
+    install_npm(HERE, build_cmd="build"), ensure_targets(jstargets),
 )
 
 
 setup_args = dict(
-    name            = name,
-    description     = 'Control JupyterLab from Python notebooks',
-    version         = version,
-    scripts         = glob(pjoin('scripts', '*')),
-    cmdclass        = cmdclass,
-    packages        = find_packages(),
-    author          = 'Jeremy Tuloup',
-    author_email    = 'jeremy@jtp.io',
-    url             = 'https://github.com/jtpio/ipylab',
-    license         = 'BSD',
-    platforms       = "Linux, Mac OS X, Windows",
-    keywords        = ['Jupyter', 'Widgets', 'IPython'],
-    classifiers     = [
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Framework :: Jupyter',
+    name=name,
+    description="Control JupyterLab from Python notebooks",
+    version=version,
+    scripts=glob(pjoin("scripts", "*")),
+    cmdclass=cmdclass,
+    packages=find_packages(),
+    author="Jeremy Tuloup",
+    author_email="jeremy@jtp.io",
+    url="https://github.com/jtpio/ipylab",
+    license="BSD",
+    platforms="Linux, Mac OS X, Windows",
+    keywords=["Jupyter", "Widgets", "IPython"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Framework :: Jupyter",
     ],
-    include_package_data = True,
-    install_requires = [
-        'ipywidgets>=7.0.0',
-    ],
-    extras_require = {
-        'test': [
-            'pytest>=3.6',
-            'pytest-cov',
-            'nbval',
-        ],
-        'examples': [
+    include_package_data=True,
+    install_requires=["ipywidgets>=7.0.0",],
+    extras_require={
+        "test": ["pytest>=3.6", "pytest-cov", "nbval",],
+        "dev": ["pre-commit", "black"],
+        "examples": [
             # Any requirements for the examples to run
         ],
-        'docs': [
-            'sphinx>=1.5',
-            'recommonmark',
-            'sphinx_rtd_theme',
-            'nbsphinx>=0.2.13,<0.4.0',
-            'jupyter_sphinx',
-            'nbsphinx-link',
-            'pytest_check_links',
-            'pypandoc',
+        "docs": [
+            "sphinx>=1.5",
+            "recommonmark",
+            "sphinx_rtd_theme",
+            "nbsphinx>=0.2.13,<0.4.0",
+            "jupyter_sphinx",
+            "nbsphinx-link",
+            "pytest_check_links",
+            "pypandoc",
         ],
     },
-    entry_points = {
-    },
+    entry_points={},
 )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup(**setup_args)
