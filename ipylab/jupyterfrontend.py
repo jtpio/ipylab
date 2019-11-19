e1c6735d0e6226254e27b38a8c2efeb702ed06cc
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2019-11-19 20:37:31 +0100

    Add hook for ready event

--- ipylab/jupyterfrontend.py
+++ ipylab/jupyterfrontend.py
@@ -4,7 +4,9 @@
 # Copyright (c) Jeremy Tuloup.
 # Distributed under the terms of the Modified BSD License.
 
-from ipywidgets import Widget, widget_serialization
+import asyncio
+
+from ipywidgets import CallbackDispatcher, Widget, widget_serialization
 from traitlets import Instance, Unicode
 from ._frontend import module_name, module_version
 
@@ -13,15 +15,27 @@ from .shell import Shell
 
 
 class JupyterFrontEnd(Widget):
-    """TODO: Make Singleton?
-    """
-
     _model_name = Unicode("JupyterFrontEndModel").tag(sync=True)
     _model_module = Unicode(module_name).tag(sync=True)
     _model_module_version = Unicode(module_version).tag(sync=True)
 
+    version = Unicode(read_only=True).tag(sync=True)
     shell = Instance(Shell).tag(sync=True, **widget_serialization)
     commands = Instance(CommandRegistry).tag(sync=True, **widget_serialization)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, shell=Shell(), commands=CommandRegistry(), **kwargs)
+        self._ready_event = asyncio.Event()
+        self._on_ready_callbacks = CallbackDispatcher()
+        self.on_msg(self._on_frontend_msg)
+
+    def _on_frontend_msg(self, _, content, buffers):
+        if content.get("event", "") == "lab_ready":
+            self._ready_event.set()
+            self._on_ready_callbacks()
+
+    async def ready(self):
+        await self._ready_event.wait()
+
+    def on_ready(self, callback, remove=False):
+        self._on_ready_callbacks.register_callback(callback, remove)
