e3bed381edcbf1ca0a23429509c8c0646f0db8a8
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2020-01-25 18:14:50 +0100

    Add custom commands to the registry

--- ipylab/commands.py
+++ ipylab/commands.py
@@ -1,8 +1,11 @@
 # Copyright (c) Jeremy Tuloup.
 # Distributed under the terms of the Modified BSD License.
 
-from ipywidgets import Widget
+from collections import defaultdict
+
+from ipywidgets import CallbackDispatcher, Widget
 from traitlets import List, Unicode
+
 from ._frontend import module_name, module_version
 
 
@@ -13,9 +16,36 @@ class CommandRegistry(Widget):
 
     _commands = List(Unicode, read_only=True).tag(sync=True)
 
-    def execute(self, command, args=None):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._execute_callbacks = defaultdict(CallbackDispatcher)
+        self.on_msg(self._on_frontend_msg)
+
+    def _on_frontend_msg(self, _, content, buffers):
+        if content.get("event", "") == "execute":
+            command_id = content.get("id")
+            self._execute_callbacks[command_id]()
+
+    def execute(self, command_id, args=None):
         args = args or {}
-        self.send({"func": "execute", "payload": {"command": command, "args": args}})
+        self.send({"func": "execute", "payload": {"id": command_id, "args": args}})
 
     def list_commands(self):
         return self._commands
+
+    def add_command(self, command_id, execute, *, caption="", label="", icon_class=""):
+        if command_id in self._execute_callbacks or command_id in self._commands:
+            raise Exception(f"Command {command_id} is already registered")
+        # TODO: support other parameters (isEnabled, isVisible...)
+        self._execute_callbacks[command_id].register_callback(execute, False)
+        self.send(
+            {
+                "func": "addCommand",
+                "payload": {
+                    "id": command_id,
+                    "caption": caption,
+                    "label": label,
+                    "iconClass": icon_class,
+                },
+            }
+        )
--- src/widgets/commands.ts
+++ src/widgets/commands.ts
@@ -20,21 +20,52 @@ export class CommandRegistryModel extends WidgetModel {
   initialize(attributes: any, options: any) {
     this.commands = CommandRegistryModel._commands;
     super.initialize(attributes, options);
-    this.on('msg:custom', this.onMessage.bind(this));
+    this.on('msg:custom', this._onMessage.bind(this));
 
     this.set('_commands', this.commands.listCommands());
     this.save_changes();
   }
 
-  private onMessage(msg: any) {
+  private _onMessage(msg: any) {
     switch (msg.func) {
       case 'execute':
-        const { command, args } = msg.payload;
-        void this.commands.execute(command, args);
+        this._execute(msg.payload);
+        break;
+      case 'addCommand':
+        void this._addComnand(msg.payload);
         break;
       default:
         break;
     }
+  }
+
+  private _execute(payload: any) {
+    const { id, args } = payload;
+    const name = `${id}-${this.model_id}`;
+    void this.commands.execute(name, args);
+  }
+
+  private async _addComnand(payload: any): Promise<void> {
+    // TODO: keep track of all the user commands,
+    // and dispose them all when the model is destroyed
+    const { id, caption, label, iconClass } = payload;
+    const name = `${id}-${this.model_id}`;
+    if (this.commands.hasCommand(name)) {
+      return;
+    }
+    void this.commands.addCommand(name, {
+      caption,
+      label,
+      iconClass,
+      execute: () => {
+        if (!this.comm_live) {
+          console.log('TODO: dispose the command');
+          return;
+        }
+        this.send({ event: 'execute', id }, {});
+      }
+    });
+    this.commands.notifyCommandChanged();
   }
 
   static serializers: ISerializers = {
