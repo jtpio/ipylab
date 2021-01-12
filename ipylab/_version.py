3092b89f4e69902b60d8a8c3ad43279699271459
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-01-12 15:28:19 +0100

    Release 0.4.0

--- ipylab/_version.py
+++ ipylab/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) ipylab contributors.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 3, 0)
+version_info = (0, 4, 0)
 __version__ = ".".join(map(str, version_info))
