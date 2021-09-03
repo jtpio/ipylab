74127366c0fae7483afff21c0e97ad04098cc662
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-09-03 23:28:18 +0200

    Update

--- ipylab/__init__.py
+++ ipylab/__init__.py
@@ -4,13 +4,14 @@
 # Copyright (c) ipylab contributors.
 # Distributed under the terms of the Modified BSD License.
 
+# import version first: https://github.com/pypa/setuptools/issues/1724#issuecomment-627241822
+from ._version import __version__, version_info
+
 import json
 import os.path as osp
 
 from .jupyterfrontend import JupyterFrontEnd
 from .widgets import Panel, SplitPanel
-
-from ._version import __version__, version_info
 
 HERE = osp.abspath(osp.dirname(__file__))
 
