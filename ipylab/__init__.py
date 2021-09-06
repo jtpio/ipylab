faa2c22bd450fd39fc98ae3934601ee041870543
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-09-06 15:03:05 +0200

    Fix handling of version

--- ipylab/__init__.py
+++ ipylab/__init__.py
@@ -4,11 +4,11 @@
 # Copyright (c) ipylab contributors.
 # Distributed under the terms of the Modified BSD License.
 
-# import version first: https://github.com/pypa/setuptools/issues/1724#issuecomment-627241822
-from ._version import __version__, version_info
-
 import json
 from pathlib import Path
+
+# import version first: https://github.com/pypa/setuptools/issues/1724#issuecomment-627241822
+from ._version import __version__, version_info
 
 from .jupyterfrontend import JupyterFrontEnd
 from .widgets import Panel, SplitPanel
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
+import json
 from pathlib import Path
 
 import setuptools
@@ -15,24 +16,35 @@ NAME = "ipylab"
 lab_path = HERE / NAME / "labextension"
 
 # Representative files that should exist after a successful build
-ensured_targets = [str(lab_path / "static" / "style.js")]
+ensured_targets = [
+    str(lab_path / "static" / "style.js"),
+    str(lab_path / "package.json"),
+]
 
 data_files_spec = [
-    (f"share/jupyter/labextensions/{NAME}", str(lab_path), "**"),
+    (f"share/jupyter/labextensions/{NAME}", str(lab_path.relative_to(HERE)), "**"),
     (f"share/jupyter/labextensions/{NAME}", str(HERE), "install.json"),
 ]
+
+# Get the package info from package.json
+pkg_json = json.loads((HERE / "package.json").read_bytes())
 
 try:
     from jupyter_packaging import wrap_installers, npm_builder, get_data_files
 
     # In develop mode, just run yarn
-    builder = npm_builder(build_cmd="build", npm="jlpm", force=True)
+    builder = npm_builder(build_cmd="build", build_dir=lab_path, source_dir="src")
     cmdclass = wrap_installers(post_develop=builder, ensured_targets=ensured_targets)
 
-    setup_args = dict(cmdclass=cmdclass, data_files=get_data_files(data_files_spec))
+    setup_args = dict(
+        cmdclass=cmdclass,
+        data_files=get_data_files(data_files_spec),
+    )
 except ImportError:
     setup_args = dict()
 
+
+setup_args["version"] = pkg_json["version"]
 
 if __name__ == "__main__":
     setuptools.setup(**setup_args)
