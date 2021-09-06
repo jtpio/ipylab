09680c96670717938927a70ed9460fb4abc0d805
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-09-06 15:53:28 +0200

    Adopt the Jupyter Releaser (#73)
    * Add check-release CI workflow
    
    * Add release hooks
    
    * More releaser config
    
    * Fix pyproject.toml
    
    * Read version from package.json
    
    * Lint
    
    * Expose version_info
    
    * Lint
    
    * Use pathlib
    
    * Run jlpm
    
    * Fix handling of version
    
    * cleanup
    
    * Missing ignore
    
    * Remove unused tests for now

--- ipylab/__init__.py
+++ ipylab/__init__.py
@@ -4,18 +4,18 @@
 # Copyright (c) ipylab contributors.
 # Distributed under the terms of the Modified BSD License.
 
+import json
+from pathlib import Path
+
 # import version first: https://github.com/pypa/setuptools/issues/1724#issuecomment-627241822
 from ._version import __version__, version_info
-
-import json
-import os.path as osp
 
 from .jupyterfrontend import JupyterFrontEnd
 from .widgets import Panel, SplitPanel
 
-HERE = osp.abspath(osp.dirname(__file__))
+HERE = Path(__file__).parent.resolve()
 
-with open(osp.join(HERE, "labextension", "package.json")) as fid:
+with open(str(HERE / "labextension" / "package.json")) as fid:
     data = json.load(fid)
 
 
--- ipylab/_version.py
+++ ipylab/_version.py
@@ -4,5 +4,32 @@
 # Copyright (c) ipylab contributors.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 4, 1)
-__version__ = ".".join(map(str, version_info))
+import re
+import json
+from pathlib import Path
+
+__all__ = ["__version__"]
+
+
+def _fetchVersion():
+    HERE = Path(__file__).parent.resolve()
+
+    for settings in HERE.rglob("package.json"):
+        try:
+            with settings.open() as f:
+                return json.load(f)["version"]
+        except FileNotFoundError:
+            pass
+
+    raise FileNotFoundError(f"Could not find package.json under dir {HERE!s}")
+
+
+__version__ = _fetchVersion()
+
+# Build up version_info tuple for backwards compatibility
+pattern = r"(?P<major>\d+).(?P<minor>\d+).(?P<patch>\d+)(?P<rest>.*)"
+match = re.match(pattern, __version__)
+parts = [int(match[part]) for part in ["major", "minor", "patch"]]
+if match["rest"]:
+    parts.append(match["rest"])
+version_info = tuple(parts)
