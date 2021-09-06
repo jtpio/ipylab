17318aba38bf32d8e87866fef4784d02162d5d4c
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2021-09-06 11:07:18 +0200

    Lint

--- ipylab/_version.py
+++ ipylab/_version.py
@@ -27,9 +27,9 @@ def _fetchVersion():
 __version__ = _fetchVersion()
 
 # Build up version_info tuple for backwards compatibility
-pattern = r'(?P<major>\d+).(?P<minor>\d+).(?P<patch>\d+)(?P<rest>.*)'
+pattern = r"(?P<major>\d+).(?P<minor>\d+).(?P<patch>\d+)(?P<rest>.*)"
 match = re.match(pattern, __version__)
-parts = [int(match[part]) for part in ['major', 'minor', 'patch']]
-if match['rest']:
-    parts.append(match['rest'])
+parts = [int(match[part]) for part in ["major", "minor", "patch"]]
+if match["rest"]:
+    parts.append(match["rest"])
 version_info = tuple(parts)
